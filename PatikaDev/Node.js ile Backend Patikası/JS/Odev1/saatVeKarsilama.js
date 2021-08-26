1b93c56cfcf73c4dc22bf66779695037360db1d4
Emre BOZAN <emrebozan8854@outlook.com>
2021-08-26 18:38:42 +0300

    kod satırları düzeltildi

--- PatikaDev/Node.js ile Backend Patikası/JS/Odev1/saatVeKarsilama.js
+++ PatikaDev/Node.js ile Backend Patikası/JS/Odev1/saatVeKarsilama.js
@@ -4,29 +4,32 @@ isimBilgisi.innerHTML=`Merhaba ${isimAl} ! Hoş Geldin!`;
 
 function saatVeTarih()
 {
-  const tarih=new Date();
-  let gunler=["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
-  let tarihBilgisi=document.querySelector("#tarihBilgisi");
+    const tarih=new Date();
+    let gunler=["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
+    let tarihBilgisi=document.querySelector("#tarihBilgisi");
+    
+    let saat=tarih.getHours();
+    let dakika=tarih.getMinutes();
+    let saniye=tarih.getSeconds();
   
-  let saat=tarih.getHours();
-  let dakika=tarih.getMinutes();
-  let saniye=tarih.getSeconds();
-  
-if (saat<10)
-{
-    saat=`0${saat}`;
-}
-if (dakika<10)
-{
-    dakika=`0${dakika}`;
-}
-if (saniye<10)
-{
-    saniye=`0${saniye}`;
-}
+    if (saat<10)
+    {
+        saat=`0${saat}`;
+    }
+
+    if (dakika<10)
+    {
+        dakika=`0${dakika}`;
+    }
+
+    if (saniye<10)
+    {
+        saniye=`0${saniye}`;
+    }
  
   
-  tarihBilgisi.innerHTML=`${saat}:${dakika}:${saniye} ${gunler[tarih.getDay()]}`;                              
+    tarihBilgisi.innerHTML=`${saat}:${dakika}:${saniye} ${gunler[tarih.getDay()]}`;    
+                              
 }
 
 setInterval(saatVeTarih, 1000); 
